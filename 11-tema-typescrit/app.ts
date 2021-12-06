6915ac7826a49e953f09d44c6721c69c40983da3
Gustavo <gustavo.kildegaard@gmail.com>
2021-12-06 19:50:06 -0300

    ejercicios de typescript

--- 11-tema-typescrit/app.ts
+++ 11-tema-typescrit/app.ts
@@ -6,3 +6,49 @@ console.log(mensaje);
 let nombre: string = 'Gus';
 mensaje = 'Mi nombre es ${nombre}'
 console.log(mensaje);
+
+enum gatos {
+    pepe,
+    coco,
+    chuzo,
+    pulu
+}
+
+var array: number[] = [1, 2, 3, 4, 5];
+console.log(array[2])
+
+var otroArray = [1, 2, 3, 4];
+console.log(otroArray[0])
+
+var [x, y, ...etc] = array;
+console.log(etc[0]);
+console.log(typeof etc, typeof etc[0], typeof array, typeof gatos, typeof gatos.pepe)
+
+let persona = { nombre: 'Gus', edad: 33 };
+console.log(persona['nombre']);
+console.log(persona.nombre);
+let msg = `Hola, soy ${persona.nombre} y tengo ${persona.edad} años`;
+console.log(msg);
+
+class Persona {
+    // Vamos a ver si me acuerdo cómo es la estructura de una clase en TS
+    readonly nombre: string;
+    readonly apellido: string;
+    private añoNac: number = 1988;
+
+    constructor(nom: string, ape: string) {
+        this.nombre = nom;
+        this.apellido = ape;
+    }
+
+    public toString(): string {
+        return this.nombre + this.apellido;
+    }
+    public edad(añoActual: number): number {
+        return añoActual - this.añoNac;
+    }
+}
+
+let gus: Persona = new Persona('Gustavo', 'Kildegaard');
+
+console.log(`Soy ${gus.nombre} ${gus.apellido} y tengo ${gus.edad(2021)} años.`);
--- 11-tema-typescrit/output/app.js
+++ 11-tema-typescrit/output/app.js
@@ -3,5 +3,39 @@
 let mensaje = 'Hola, este es mi primer TS!';
 console.log(mensaje);
 let nombre = 'Gus';
-mensaje = `Mi nombre es ${nombre}`;
+mensaje = 'Mi nombre es ${nombre}';
 console.log(mensaje);
+var gatos;
+(function (gatos) {
+    gatos[gatos["pepe"] = 0] = "pepe";
+    gatos[gatos["coco"] = 1] = "coco";
+    gatos[gatos["chuzo"] = 2] = "chuzo";
+    gatos[gatos["pulu"] = 3] = "pulu";
+})(gatos || (gatos = {}));
+var array = [1, 2, 3, 4, 5];
+console.log(array[2]);
+var otroArray = [1, 2, 3, 4];
+console.log(otroArray[0]);
+var [x, y, ...etc] = array;
+console.log(etc[0]);
+console.log(typeof etc, typeof etc[0], typeof array, typeof gatos, typeof gatos.pepe);
+let persona = { nombre: 'Gus', edad: 33 };
+console.log(persona['nombre']);
+console.log(persona.nombre);
+let msg = `Hola, soy ${persona.nombre} y tengo ${persona.edad} años`;
+console.log(msg);
+class Persona {
+    constructor(nom, ape) {
+        this.añoNac = 1988;
+        this.nombre = nom;
+        this.apellido = ape;
+    }
+    toString() {
+        return this.nombre + this.apellido;
+    }
+    edad(añoActual) {
+        return añoActual - this.añoNac;
+    }
+}
+let gus = new Persona('Gustavo', 'Kildegaard');
+console.log(`Soy ${gus.nombre} ${gus.apellido} y tengo ${gus.edad(2021)} años.`);
